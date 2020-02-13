42016e2cfae5da8d6a48ca41a7f6e67d95d7a806
Asuu <suqinzheng_0624@163.com>
2020-02-13 11:22:08 +0800

    [Asu]feat: add a class ChromeF5 to output refresh page

--- 006_polymorphism/src/main/java/Application.java
+++ 006_polymorphism/src/main/java/Application.java
@@ -1,6 +1,8 @@
 public class Application {
     public static void main(String[] args) {
         WindowsF5 intellijIdea = new IntellijIdeaF5();
+        WindowsF5 chrome = new ChromeF5();
         intellijIdea.pressF5();
+        chrome.pressF5();
     }
 }
---
+++ 006_polymorphism/src/main/java/ChromeF5.java
@@ -0,0 +1,6 @@
+public class ChromeF5 extends WindowsF5 {
+    @Override
+    public void pressF5() {
+        System.out.println("刷新网页");
+    }
+}
