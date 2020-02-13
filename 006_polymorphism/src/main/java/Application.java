9cec52e20380ea173ff0f83efcbc99992acb10c9
Asuu <suqinzheng_0624@163.com>
2020-02-13 11:18:11 +0800

    [Asu]feat: add a class IntellijIdeaF5 to output copy

--- 006_polymorphism/src/main/java/Application.java
+++ 006_polymorphism/src/main/java/Application.java
@@ -1,5 +1,6 @@
 public class Application {
     public static void main(String[] args) {
-
+        WindowsF5 intellijIdea = new IntellijIdeaF5();
+        intellijIdea.pressF5();
     }
 }
---
+++ 006_polymorphism/src/main/java/IntellijIdeaF5.java
@@ -0,0 +1,6 @@
+public class IntellijIdeaF5 extends WindowsF5 {
+    @Override
+    public void pressF5() {
+        System.out.println("弹出copy当前文件弹窗");
+    }
+}
