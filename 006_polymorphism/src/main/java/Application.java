c11fe26aa5b8e6804f7d762382ad014ff6bb0c19
Asuu <suqinzheng_0624@163.com>
2020-02-13 11:29:37 +0800

    [Asu]feat: add a class WechatF5 to output input box

--- 006_polymorphism/src/main/java/Application.java
+++ 006_polymorphism/src/main/java/Application.java
@@ -2,7 +2,10 @@ public class Application {
     public static void main(String[] args) {
         WindowsF5 intellijIdea = new IntellijIdeaF5();
         WindowsF5 chrome = new ChromeF5();
+        WindowsF5 wechat = new WechatF5();
+
         intellijIdea.pressF5();
         chrome.pressF5();
+        wechat.pressF5();
     }
 }
---
+++ 006_polymorphism/src/main/java/WechatF5.java
@@ -0,0 +1,7 @@
+public class WechatF5 extends WindowsF5 {
+    @Override
+    public void pressF5() {
+        super.pressF5();
+        System.out.println("弹出英文输出框");
+    }
+}
